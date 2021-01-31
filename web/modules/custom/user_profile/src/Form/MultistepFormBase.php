edb351a685dca246b362e6a515edc9a13e2bf470
luis <luis.jimenez@bitsamericas.com>
2021-01-31 01:44:55 -0500

    Multistep form base created

---
+++ web/modules/custom/user_profile/src/Form/MultistepFormBase.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Drupal\user_profile\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class MultistepFormBase.
+ */
+class MultistepFormBase extends FormBase {
+
+  /**
+   * Drupal\user\PrivateTempStoreFactory definition.
+   *
+   * @var \Drupal\user\PrivateTempStoreFactory
+   */
+  protected $userPrivateTempstore;
+
+  /**
+   * Drupal\Core\Session\SessionManagerInterface definition.
+   *
+   * @var \Drupal\Core\Session\SessionManagerInterface
+   */
+  protected $sessionManager;
+
+  /**
+   * Drupal\Core\Session\AccountProxyInterface definition.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * @var \Drupal\user\PrivateTempStore
+   */
+  protected $store;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->userPrivateTempstore = $container->get('user.private_tempstore');
+    $instance->sessionManager = $container->get('session_manager');
+    $instance->currentUser = $container->get('current_user');
+
+    $instance->store = $instance->userPrivateTempstore->get('multistep_data');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'multistep_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Start a manual session for anonymous users.
+    if ($this->currentUser->isAnonymous() && !isset($_SESSION['multistep_form_holds_session'])) {
+      $_SESSION['multistep_form_holds_session'] = true;
+      $this->sessionManager->start();
+    }
+
+    $form = [];
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+      '#button_type' => 'primary',
+      '#weight' => 10,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      // @TODO: Validate fields.
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Display result.
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Saves the data from the multistep form.
+   */
+  protected function saveData() {
+    // Logic for saving data goes here...
+    $this->deleteStore();
+    $this->messenger()->addStatus($this->t('The form has been saved.'));
+  }
+
+  /**
+   * Helper method that removes all the keys from the store collection used for
+   * the multistep form.
+   */
+  protected function deleteStore() {
+    $keys = ['name', 'email', 'age', 'location'];
+    foreach ($keys as $key) {
+      $this->store->delete($key);
+    }
+  }
+
+}
